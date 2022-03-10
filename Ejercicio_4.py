15c3645d29df601fc806e77d14e55c1645b69ea9
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-03-10 18:47:47 +0000

    código

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -2,3 +2,6 @@ class ordenacion_dicotomica:
     def __init__(self):
         while True:
           self.tabla = lista(tabla)
+          for i in range(len(self.tabla)):
+            for j in range(i, len(self.tabla)):
+              if self.tabla[i] > self.tabla[j]
