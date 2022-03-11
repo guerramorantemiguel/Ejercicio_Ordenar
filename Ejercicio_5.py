e4c92982a6064887eeda09bb28a80f31139a0cf6
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-03-11 17:56:21 +0000

    código

--- Ejercicio_5.py
+++ Ejercicio_5.py
@@ -1,3 +1,9 @@
 import random
 
 class ordenacion_tareas:
+    def __init__(self,tareas):
+        self.tareas = defaultdict(tareas) 
+        self.T = tareas
+    def ordenacio_tareas(tareas):
+      for j in range(len(tareas)):
+  
