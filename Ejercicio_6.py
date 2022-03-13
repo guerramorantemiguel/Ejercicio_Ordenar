a3e5058c1871a18efd40616c0a0ea8953e9428aa
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-03-13 19:13:47 +0000

    ejercicio 6 no sé como terminarlo y lo que he puesto no sé si está bien.

--- Ejercicio_6.py
+++ Ejercicio_6.py
@@ -4,7 +4,15 @@ class segmento:
     def __init__(self):
         while True:
           self.tabla = defaultdict(list)
-          
-
-
-
+    
+    def limite(self, u, v):
+        self.graph[u].append(v)
+ 
+    def topologicalSortUtil(self, v, visited, stack):
+        visited[v] = True
+ 
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.topologicalSortUtil(i, visited, stack)
+ 
+ 
